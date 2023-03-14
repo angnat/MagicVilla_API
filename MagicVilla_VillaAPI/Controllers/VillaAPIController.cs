fe57d0d4739fe57c736de6603ece013d59d89400
angnat <angnat05@yahoo.com>
2023-03-14 16:53:10 +0700

    New Controller

---
+++ MagicVilla_VillaAPI/Controllers/VillaAPIController.cs
@@ -0,0 +1,24 @@
+﻿using MagicVilla_VillaAPI.Data;
+using MagicVilla_VillaAPI.Models;
+using MagicVilla_VillaAPI.Models.Dto;
+using Microsoft.AspNetCore.Mvc;
+
+namespace MagicVilla_VillaAPI.Controllers
+{
+    [Route("api/VillaAPI")]
+    [ApiController]
+    public class VillaAPIController : ControllerBase
+    {
+        [HttpGet]
+        public IEnumerable<VillaDto> GetVillas()
+        {
+            return VillaStore.villaList;
+        }
+
+        [HttpGet("Id")]
+        public VillaDto GetVilla(int id)
+        {
+            return VillaStore.villaList.FirstOrDefault(u=>u.Id==id);
+        }
+    }
+}
