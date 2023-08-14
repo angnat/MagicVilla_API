aa4dacd98793066c05ae0073721d29fa2c12557b
angnat <angnat05@yahoo.com>
2023-08-14 11:26:30 +0700

    IVillaService Controller etc

---
+++ MagicVilla_Web/Models/VillaService.cs
@@ -0,0 +1,64 @@
+﻿using MagicVilla_Utility;
+using MagicVilla_Web.Models.Dto;
+using MagicVilla_Web.Services;
+using MagicVilla_Web.Services.IServices;
+
+namespace MagicVilla_Web.Models
+{
+    public class VillaService : BaseService, IVillaService
+    {
+        private readonly IHttpClientFactory _httpClientFactory;
+        private string villaURL;
+        private VillaService(IHttpClientFactory clientFactory, IConfiguration configuration) : base(clientFactory)
+        {
+            _httpClientFactory = clientFactory;
+            villaURL = configuration.GetValue<string>("ServiceUrls:VillaAPI");
+        }
+        public Task<T> CreateAsync<T>(VillaCreateDto dto)
+        {
+            return SendAsync<T>(new APIRequest()
+            {
+                ApiType=SD.ApiType.POST,
+                Data = dto,
+                Url = villaURL + "/api/villaAPI"
+            });
+        }
+
+        public Task<T> DeleteAsync<T>(int id)
+        {
+            return SendAsync<T>(new APIRequest()
+            {
+                ApiType = SD.ApiType.DELETE,
+                Url = villaURL + "/api/villaAPI/" + id
+            });
+        }
+      
+        public Task<T> GetAllAsync<T>()
+        {
+            return SendAsync<T>(new APIRequest()
+            {
+                ApiType = SD.ApiType.GET,              
+                Url = villaURL + "/api/villaAPI"
+            });
+        }
+
+        public Task<T> GetAsync<T>(int id)
+        {
+            return SendAsync<T>(new APIRequest()
+            {
+                ApiType = SD.ApiType.GET,               
+                Url = villaURL + "/api/villaAPI/" + id
+            });
+        }
+
+        public Task<T> UpdateAsync<T>(VillaUpdateDto dto)
+        {
+            return SendAsync<T>(new APIRequest()
+            {
+                ApiType = SD.ApiType.PUT,
+                Data = dto,
+                Url = villaURL + "/api/villaAPI/" + dto.Id
+            });
+        }
+    }
+}
--- MagicVilla_Web/Services/BaseService.cs
+++ MagicVilla_Web/Services/BaseService.cs
@@ -15,7 +15,7 @@ namespace MagicVilla_Web.Services
             this.httpClientFactory = httpClientFactory;
         }
 
-        public Task<T> SendAsync<T>(APIRequest apiRequest)
+        public async Task<T> SendAsync<T>(APIRequest apiRequest)
         {
             try
             {
@@ -28,6 +28,38 @@ namespace MagicVilla_Web.Services
                     message.Content = new StringContent(JsonConvert.SerializeObject(apiRequest.Data),System.Text.Encoding.UTF8,
                         "application/json");
                 }
+                switch(apiRequest.ApiType)
+                {
+                    case MagicVilla_Utility.SD.ApiType.POST:
+                        message.Method = HttpMethod.Post;
+                        break;
+                    case MagicVilla_Utility.SD.ApiType.PUT:
+                        message.Method = HttpMethod.Put;
+                        break;
+                    case MagicVilla_Utility.SD.ApiType.DELETE:
+                        message.Method = HttpMethod.Delete;
+                        break;
+                    case MagicVilla_Utility.SD.ApiType.GET:
+                        message.Method = HttpMethod.Get;
+                        break;
+                }
+                HttpResponseMessage apiResponse = null;
+                apiResponse = await client.SendAsync(message);
+                var apiContent = await apiResponse.Content.ReadAsStringAsync();
+                var APIResponse = JsonConvert.DeserializeObject<T>(apiContent);
+                return APIResponse;
+            }
+            catch(Exception ex)
+            {
+                var dto = new APIResponse
+                {
+                    ErrorsMessage = new List<string> { Convert.ToString(ex.Message) },
+                    IsSuccess = false
+                };
+
+            var res = JsonConvert.SerializeObject(dto);
+            var APIResponse = JsonConvert.DeserializeObject<T>(res);
+                return APIResponse;
             }
         }
     }
